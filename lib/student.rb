adae09e6f80ae6df153203a8a7b1c5fc6343326c
Katie Le <lethikatie@gmail.com>
2020-09-08 22:40:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,11 +29,11 @@ sql =  <<-SQL
   end
   
   def save
-        sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+        VALUES (?, ?)
     SQL
- 
+
     DB[:conn].execute(sql, self.name, self.grade)
   end
 end 
