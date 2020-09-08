907108869802260aa8a99cf493ef5463a4c7b987
Katie Le <lethikatie@gmail.com>
2020-09-08 22:36:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,10 +19,12 @@ sql =  <<-SQL
     DB[:conn].execute(sql)
    end 
    
-   def self.drop_table 
-     sql = <<-SQL
-     DROP TABLE students 
-     SQL 
-     DB[:conn].execute(sql)
-   end 
+   
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+    SQL
+
+    DB[:conn].execute(sql)  
+  end
  end 
